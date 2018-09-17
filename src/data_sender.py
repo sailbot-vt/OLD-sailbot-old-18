270e8b14d59945b5d1f4d1cb39d26a0bb28fec80
Ryan Singman <ryanjs@vt.edu>
2018-09-17 11:51:11 -0400

    Added documentation for data_sender ... made comment on potential changes to how to send to sockets

--- src/data_sender.py
+++ src/data_sender.py
@@ -16,14 +16,17 @@ server_addresses = [('192.168.0.100', 10101), ('192.168.0.101', 10101), ('192.16
 #sock.connect(server_address)
 
 def send_update():
+    """
+    Get status package containing boat status information from ship_data module... Set status vars in ship_data and call status package to receive dictionary... sends status package message to all server addresses defined by 'server_addresses' list 
+    """
     message = str(ship_data.status_package())
 
     # Send data
 #    print('sending "%s"' % message)
     for server_address in server_addresses:
-	    sock.sendto(message.encode(), server_address)
+	    sock.sendto(message.encode(), server_address)		#may want to connect to sockets before hand then call socket.send instead ... especially if server addresses are static
 
-if(__name__ == "__main__"):
+if(__name__ == "__main__"):		#used to test send_update method
 	for i in range(10000):
 		r = random.random()
 		ship_data.boat_lat = i*.0001*(.1 + math.sin(r - .5))
