58adaaa5755ad86167228dfc3a7b9726ea059ef0
Tom Twomey <twomey@vt.edu>
2018-09-28 12:25:18 -0400

    Added comments to stoppable_thread.py.

--- src/stoppable_thread.py
+++ src/stoppable_thread.py
@@ -1,8 +1,23 @@
 import threading
 
 class StoppableThread(threading.Thread):
+    """
+    Extends the treading class to create a thread that can be stopped.
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes the StoppableThread with some args that are passed to a
+        regular thread.
+
+        Keyword argments:
+        self -- The caller, StoppableThread instance
+        args -- arguments
+        kwargs -- other arguments
+
+        Side effects:
+        Creates the thread with the provided args and kwargs
+        """
         super(StoppableThread, self).__init__(*args, **kwargs)
         self._stop_flag = threading.Event()
 
@@ -17,7 +32,25 @@ class StoppableThread(threading.Thread):
     """
 
     def stop(self):
+        """
+        Function to stop the thread.
+
+        Keyword arguments:
+        self -- The caller to be stopped.
+
+        Side effects:
+        sets the stop flag
+        """
         self._stop_flag.set()
 
     def stopped(self):
+        """
+        Function that returns if the thread is stopped.
+
+        Keyword arguments:
+        self -- The caller to have its flag checked.
+
+        Returns:
+        If the stopped flag is set.
+        """
         return self._stop_flag.isSet()
